f1f8f7bc97019c3e2e2d1b3a50ea04f31dcfb3e7
sameobake <23693217+sameobake@users.noreply.github.com>
2022-05-20 20:29:31 +0900

    Export PPM(.ppm) Image

---
+++ 001_export_ppm_image/export_ppm_image.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+
+int main() {
+    const int image_width = 256;
+    const int image_height = 256;
+
+    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+
+    for (int i = image_height - 1; i >= 0; --i) {
+        for (int j = 0; j < image_width; ++j) {
+            auto r = double(j) / (image_width - 1);
+            auto g = double(i) / (image_height - 1);
+            auto b = 0.25;
+
+            int ir = static_cast<int>(255.999 * r);
+            int ig = static_cast<int>(255.999 * g);
+            int ib = static_cast<int>(255.999 * b);
+
+            std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+        }
+    }
+}
